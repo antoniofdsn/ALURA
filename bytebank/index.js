74d564e0958d64a7a117cb2e123a8f184a843798
antoniofdsn <antoniofdsn@live.com>
2022-04-26 23:38:10 -0300

    Finalizando curso: JS programando Orientação a Obj

--- bytebank/index.js
+++ bytebank/index.js
@@ -1,45 +1,26 @@
-class Cliente {
-  nome;
-  cpf;
-}
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
-class ContaCorrente {
-  agencia;
-  _saldo;
-  //#saldo = 0
-  //https://github.com/tc39/proposal-class-fields#private-fields
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
-  sacar(valor) {
-    if (this._saldo >= valor) {
-      this._saldo -= valor;
-      return valor;
-    }
-  }
-
-  depositar(valor) {
-    if (valor <= 0) return;
-    this._saldo += valor;
-    return valor;
-  }
-}
-
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
 contaCorrenteRicardo._saldo = 10;
 contaCorrenteRicardo.depositar(100);
 contaCorrenteRicardo.sacar(50);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+const cliente2 = new Cliente("Alice", 88822233309);
 
-const contaCorrenteAlice = new ContaCorrente();
-contaCorrenteAlice.agencia = 1001;
-contaCorrenteAlice._saldo = 0;
+const contaCorrenteAlice = new ContaCorrente(1001, cliente2);
+contaCorrenteAlice._saldo = 100;
+contaCorrenteAlice.depositar(500);
 
-console.log(cliente1, contaCorrenteRicardo);
-console.log(cliente2, contaCorrenteAlice);
+const cliente3 = new Cliente("Bel", 88822233309);
+
+const contaCorrenteBel = new ContaCorrente(1001, cliente3);
+contaCorrenteBel._saldo = 100;
+
+contaCorrenteAlice.transferir(300, contaCorrenteBel);
+
+console.log(ContaCorrente.numeroDeContas);
+console.log(contaCorrenteAlice);
+console.log(cliente3, contaCorrenteBel._saldo);
